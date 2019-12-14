d615547989d9c9203950b6404587d0f1a33f9e5c
RaymondHuang210129 <raymond860909@gmail.com>
2019-12-15 01:01:00 +0800

    HTTPServer: runs functionally

---
+++ server.cpp
@@ -0,0 +1,155 @@
+#include <array>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <map>
+#include <boost/asio.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/regex.hpp>
+#include <boost/regex.hpp>
+//#include <boost/algorithm/string/split.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <memory>
+#include <utility>
+
+using namespace std;
+using namespace boost::asio;
+
+io_service global_io_service;
+
+class HTTPSession : public enable_shared_from_this<HTTPSession> {
+private:
+	enum { max_length = 1024 };
+	ip::tcp::socket _socket;
+	//strand<io_context::executer_type> _strand;
+	array<char, max_length> _data;
+	//string const& _doc_root;
+	//http::request<http::string_body> _request;
+	//shared_ptr<void> _result;
+
+public:
+	HTTPSession(ip::tcp::socket socket) : _socket(move(socket)) {}
+	void start() { do_read(); }
+
+private:
+	void do_read() {
+		auto self(shared_from_this());
+		_socket.async_read_some(
+			buffer(_data, max_length),
+			[this, self](boost::system::error_code ec, size_t length) {
+				if (!ec) {
+					map<string, string> envmap = header_parser(_data.data());
+					cout << "start fork" << endl;
+					/* section: fork */
+					pid_t cpid = fork();
+					if (cpid == 0) {
+						for (auto iter = envmap.begin(); iter != envmap.end(); iter++) {
+							setenv(iter->first.c_str(), iter->second.c_str(), 1);
+							auto native = _socket.native_handle();
+							close(STDIN_FILENO);
+							close(STDOUT_FILENO);
+							//close(STDERR_FILENO);
+							dup2(native, STDIN_FILENO);
+							dup2(native, STDOUT_FILENO);
+							//dup2(native, STDERR_FILENO);
+							cout << "HTTP/1.1 200 OK\r\n";
+							//_socket.close();
+							//close(STDIN_FILENO);
+							//close(STDOUT_FILENO);
+							//return;
+							char* execVect[2];
+							string cgiPath = "." + envmap["REQUEST_URI"];
+							execVect[0] = strdup(cgiPath.c_str());
+							execVect[1] = NULL;
+							execvp(execVect[0], execVect);
+							if (errno == 2) {
+								cerr << "Unknown URI: [" << execVect[0] << "]." << endl;
+							}
+							else {
+								cerr << "exec error: " << errno << endl;
+							}
+							//close(_socket.native_handle());
+							//_socket.shutdown(boost::asio::ip::tcp::socket::shutdown_both);
+						}
+					} else {
+						cout << "closing parent socket" << endl;
+						_socket.close();
+						//_socket.shutdown(boost::asio::ip::tcp::socket::shutdown_both);
+					}
+				}
+			}
+		);
+	}
+
+	map<string, string> header_parser(char* rawData) {
+		string data(rawData);
+		vector<string> splitedHeader;
+		split_regex(splitedHeader, data, boost::regex("(\r\n)+"));
+		cout << splitedHeader.size() << endl;
+		//for (int i = 0; i < splitedHeader.size(); i++) {
+		//	cout << i << ":" << splitedHeader[i] << endl;
+		//}
+		map<string, string> envmap;
+		vector<string> splitedHeaderL1;
+		boost::split(splitedHeaderL1, splitedHeader[0], boost::is_any_of(" "), boost::token_compress_on);
+		envmap.insert(pair<string, string>("REQUEST_METHOD", splitedHeaderL1[0]));
+		vector<string> tmp;
+
+		boost::split(tmp, splitedHeaderL1[1], boost::is_any_of("?"), boost::token_compress_on);
+		envmap.insert(pair<string, string>("REQUEST_URI", tmp[0]));
+		if (tmp.size() == 1) {
+			envmap.insert(pair<string, string>("QUERY_STRING", ""));
+		} else {
+			envmap.insert(pair<string, string>("QUERY_STRING", tmp[1]));
+		}
+		envmap.insert(pair<string, string>("SERVER_PROTOCOL", splitedHeaderL1[2]));
+		envmap.insert(pair<string, string>("HTTP_HOST", splitedHeader[1].substr(6)));
+		envmap.insert(pair<string, string>("SERVER_ADDR", _socket.local_endpoint().address().to_string()));
+		envmap.insert(pair<string, string>("SERVER_PORT", to_string(_socket.local_endpoint().port())));
+		envmap.insert(pair<string, string>("REMOTE_ADDR", _socket.remote_endpoint().address().to_string()));
+		envmap.insert(pair<string, string>("REMOTE_PORT", to_string(_socket.remote_endpoint().port())));
+		for (auto iter = envmap.begin(); iter != envmap.end(); iter++) {
+			cout << iter->first << ": " << iter->second << endl;
+		}
+		return envmap;
+	}
+};
+
+class HTTPServer {
+private:
+	ip::tcp::acceptor _acceptor;
+	ip::tcp::socket _socket;
+
+public:
+	HTTPServer(short port) : _acceptor(global_io_service, ip::tcp::endpoint(ip::tcp::v4(), port)),
+	                         _socket(global_io_service) {
+		do_accept();
+	}
+
+private:
+	void do_accept() {
+		_acceptor.async_accept(_socket, [this](boost::system::error_code ec) {
+			if (!ec) {make_shared<HTTPSession>(move(_socket))->start();}
+			do_accept();
+		});
+	}
+};
+
+int main(int argc, char* const argv[]) {
+	if (argc != 2) {
+		cerr << "Incorrect parameters." << endl;
+		exit(1);
+	}
+
+	try {
+		unsigned short port = atoi(argv[1]);
+		HTTPServer server(port);
+		global_io_service.run();
+	} catch (exception& e) {
+		cerr << "Exception: " << e.what() << "\n";
+	}
+
+	return 0;
+}
